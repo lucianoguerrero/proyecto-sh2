f036bce2ce9df96f2530b56a5cb78e6b8b5666b3
Luciano guerrero <luciano.agt@scipion.cl>
2022-05-12 22:02:51 +0000

    [FIX] cambiando el import error

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-from odoo.exceptions import UserError, Validation
+from odoo.exceptions import UserError, ValidationError
 
 class Course(models.Model):
     
